29f3dbb050774679eed5c6ad1d822d9c17b2093d
Iurii <Iurii>
2023-04-30 22:58:24 +0500

    Logic Change RGB Colors

--- ManipulateOfRgbColors/ViewController.swift
+++ ManipulateOfRgbColors/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var labelForRed: UILabel!
+    @IBOutlet var labelForGreen: UILabel!
+    @IBOutlet var labelForBlue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coloredView.layer.cornerRadius = 10
+        
     }
-
-
+    
+    private func changeViewColor() {
+        coloredView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value),
+                                              green: CGFloat(greenSlider.value),
+                                              blue: CGFloat(blueSlider.value),
+                                              alpha: 1)
+    }
+    
+    @IBAction func valueRedSliderChange(_ sender: UISlider) {
+        labelForRed.text = String(format: "%.2f", redSlider.value)
+    }
+    @IBAction func valueGreenSliderChange(_ sender: UISlider) {
+        labelForGreen.text = String(format: "%.2f", greenSlider.value)
+    }
+    @IBAction func valueBlueSliderChange(_ sender: UISlider) {
+        labelForBlue.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    @IBAction func redSliderChange(_ sender: UISlider) {
+        changeViewColor()
+    }
+    @IBAction func greenSliderChange(_ sender: UISlider) {
+        changeViewColor()
+    }
+    @IBAction func blueSliderChange(_ sender: UISlider) {
+        changeViewColor()
+    }
+    
+    
 }
 
